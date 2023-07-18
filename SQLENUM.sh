653f01dbd27ac8af119d42564df75eb90330e129
IPv4est <122734905+IPv4est@users.noreply.github.com>
2023-07-18 21:33:01 +0100

    Update SQLENUM.sh

--- SQLENUM.sh
+++ SQLENUM.sh
@@ -14,23 +14,23 @@ nmap $IP -p $PORT --script mysql-info --append-output -oN {$IP}{$USER}SQLENUM.tx
 
 #nmap script users
 echo "Nmap script Users" >> {$IP}{$USER}SQLENUM.txt
-nmap $IP -p $PORT --script mysql-users --script-args="mysqluser='$USER',mysqlpass=$PASSWORD" --append-output -oN SQLENUM.txt
+nmap $IP -p $PORT --script mysql-users --script-args="mysqluser='$USER',mysqlpass=$PASSWORD" --append-output -oN {$IP}{$USER}SQLENUM.txt
 
 #nmap script empty passwords
 echo "Nmap script empty passwords"
-nmap $IP -p $PORT --script mysql-empty-password --append-output -oN SQLENUM.txt
+nmap $IP -p $PORT --script mysql-empty-password --append-output -oN {$IP}{$USER}SQLENUM.txt
 
 #nmap script databases
 echo "Nmap script databases" >> {$IP}{$USER}SQLENUM.txt
-nmap $IP -p $PORT --script mysql-databases --script-args="mysqluser='$USER',mysqlpass=$PASSWORD" --append-output -oN SQLENUM.txt
+nmap $IP -p $PORT --script mysql-databases --script-args="mysqluser='$USER',mysqlpass=$PASSWORD" --append-output -oN {$IP}{$USER}SQLENUM.txt
 
 #nmap script variables
 echo "Nmap script variables" >> {$IP}{$USER}SQLENUM.txt
-nmap $IP -p $PORT --script mysql-variables --script-args="mysqluser='$USER',mysqlpass=$PASSWORD" --append-output -oN SQLENUM.txt
+nmap $IP -p $PORT --script mysql-variables --script-args="mysqluser='$USER',mysqlpass=$PASSWORD" --append-output -oN {$IP}{$USER}SQLENUM.txt
 
 
 #nmap script audit
 echo "Nmap script audit" >> {$IP}{$USER}SQLENUM.txt
-nmap $IP -p $PORT --script mysql-audit --script-args "mysql-audit.username='$USER',mysql-audit.password=$PASSWORD,mysql-audit.filename='/usr/share/nmap/nselib/data/mysql-cis.audit'" --append-output -oN SQLENUM.txt
+nmap $IP -p $PORT --script mysql-audit --script-args "mysql-audit.username='$USER',mysql-audit.password=$PASSWORD,mysql-audit.filename='/usr/share/nmap/nselib/data/mysql-cis.audit'" --append-output -oN {$IP}{$USER}SQLENUM.txt
 
 
